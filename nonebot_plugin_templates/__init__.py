8374887c3b4f4507ff7da2c56b11a0eea52924c5
canxin <1969730106@qq.com>
2023-07-18 11:42:09 +0800

    update

---
+++ nonebot_plugin_templates/__init__.py
@@ -0,0 +1,8 @@
+from nonebot.plugin import PluginMetadata
+
+__plugin_meta__ = PluginMetadata(
+    name="templates_render",
+    description="使用htmlrender和jinja2渲染,使用构建的menu,card或dict进行模板渲染",
+    usage="import 后构建相应类并进行渲染得到bytes的图片",
+    extra={},
+)
